b9423badd29d575703269c1371c8d77e60f67190
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-02-08 11:10:22 -0500

    Update PrimaryButtonEnglish.jsx

--- src/app/components/componentsEnglish/PrimaryButtonEnglish.jsx
+++ src/app/components/componentsEnglish/PrimaryButtonEnglish.jsx
@@ -1,12 +1,15 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 export const PrimaryButtonEnglish = ({ islink }) => {
+  const path = usePathname();
+
   if (islink) {
     return (
       <a
-        className=" w-[100px] h-[50px]  text-complementario p-3 rounded-md flex gap-2 justify-center items-center  shadow-inner shadow-complementario hover:scale-105 trasition duration-500 cursor-pointer hover:shadow-primario "
+        className=" w-[100px] h-[50px] text-complementario p-3 rounded-md flex gap-2 justify-center items-center  shadow-inner shadow-complementario hover:scale-105 trasition duration-500 cursor-pointer hover:shadow-primario "
         href={islink}
         target="_blank"
       >
@@ -15,8 +18,8 @@ export const PrimaryButtonEnglish = ({ islink }) => {
     );
   } else {
     return (
-      <Link href={"/en#proyects_english"}>
-        <button className="w-[100px] h-[50px]  text-complementario  rounded-md flex gap-2 justify-center items-center  shadow-inner shadow-complementario hover:scale-105 trasition duration-500 cursor-pointer hover:shadow-primario ">
+      <Link href={path.includes === "/en" ? "/en/#proyects" : "/#proyects"}>
+        <button className=" w-[100px] h-[50px] text-complementario  rounded-md flex gap-2 justify-center items-center  shadow-inner shadow-complementario hover:scale-105 trasition duration-500 cursor-pointer hover:shadow-primario ">
           Back
         </button>
       </Link>
