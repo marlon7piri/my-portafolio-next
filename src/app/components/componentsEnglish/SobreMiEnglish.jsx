9d6664c294f3ba6650ec3701ea8a2cb77bba864b
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-03-09 08:56:51 -0500

    Update SobreMiEnglish.jsx

--- src/app/components/componentsEnglish/SobreMiEnglish.jsx
+++ src/app/components/componentsEnglish/SobreMiEnglish.jsx
@@ -12,13 +12,13 @@ const SobreMiEnglish = () => {
           <b className="text-primario"> FullStack programmer</b>, self-taught,
           and currently pursuing a degree in Computer Systems Engineering.
           <br></br>
-          <br></br>I started programming 5 years ago when I discovered the wonders that can be achieved with coding,
-          and I enjoy it so much that it became my hobby.
-          I used to spend any free time I had programming,
-          and I love learning new things,
-          so I listen to constructive advice.
-          I have knowledge of programming languages for both backend and frontend development.
-          On my profile, you can see individual projects I have worked on.
+          <br></br>I started programming 5 years ago when I discovered the
+          wonders that can be achieved with coding, and I enjoy it so much that
+          it became my hobby. I used to spend any free time I had programming,
+          and I love learning new things, so I listen to constructive advice. I
+          have knowledge of programming languages for both backend and frontend
+          development. On my profile, you can see individual projects I have
+          worked on.
         </p>
 
         <div className="w-4 h-4 absolute -top-2 -left-2 bg-primario rounded-full"></div>
